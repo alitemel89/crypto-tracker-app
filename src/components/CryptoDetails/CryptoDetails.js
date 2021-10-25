40aca1a75a6fc8add2e3e22fe90b76ca936b7faa
alitemel89 <alitemel89.ce@gmail.com>
2021-10-25 17:21:51 +0300

    update styles at cryptocurrencydetails page

--- src/components/CryptoDetails/CryptoDetails.js
+++ src/components/CryptoDetails/CryptoDetails.js
@@ -3,7 +3,26 @@ import { useParams } from "react-router-dom";
 import millify from "millify";
 
 import { useGetCryptoDetailsQuery } from "../../services/cryptoApi";
-import { AiFillDollarCircle, AiOutlineNumber } from "react-icons/ai";
+import {
+  AiFillDollarCircle,
+  AiOutlineNumber,
+  AiOutlineThunderbolt,
+  AiFillTrophy,
+  AiFillStop,
+  AiFillCheckCircle,
+} from "react-icons/ai";
+
+import {
+  TextWrapper,
+  InfoColumn,
+  TopLine,
+  Heading,
+  Subtitle,
+  InfoSec,
+  InfoRow,
+  TableRow,
+  TableValue,
+} from "../CryptoDetails/CryptoDetails.styles";
 
 const CryptoDetails = () => {
   const [timePeriod, setTimePeriod] = useState("7d");
@@ -29,9 +48,96 @@ const CryptoDetails = () => {
       value: cryptoDetails.rank,
       icon: <AiOutlineNumber />,
     },
+    {
+      title: "24h Volume",
+      value: `${cryptoDetails.volume && millify(cryptoDetails.volume)}`,
+      icon: <AiOutlineThunderbolt />,
+    },
+    {
+      title: "Market Cap",
+      value: `$ ${millify(cryptoDetails.marketCap)}`,
+      icon: <AiFillDollarCircle />,
+    },
+    {
+      title: "All-time-high(daily avg.)",
+      value: `$ ${millify(cryptoDetails.allTimeHigh.price)}`,
+      icon: <AiFillTrophy />,
+    },
   ];
 
-  return <div>CryptoDetails {coinId}</div>;
+  const genericStats = [
+    {
+      title: "Number Of Markets",
+      value: cryptoDetails.numberOfMarkets,
+      icon: <AiOutlineThunderbolt />,
+    },
+    {
+      title: "Number Of Exchanges",
+      value: cryptoDetails.numberOfExchanges,
+      icon: <AiOutlineThunderbolt />,
+    },
+    {
+      title: "Aprroved Supply",
+      value: cryptoDetails.approvedSupply ? (
+        <AiFillCheckCircle />
+      ) : (
+        <AiFillStop />
+      ),
+      icon: <AiOutlineThunderbolt />,
+    },
+    {
+      title: "Total Supply",
+      value: `$ ${millify(cryptoDetails.totalSupply)}`,
+      icon: <AiOutlineThunderbolt />,
+    },
+    {
+      title: "Circulating Supply",
+      value: `$ ${millify(cryptoDetails.circulatingSupply)}`,
+      icon: <AiOutlineThunderbolt />,
+    },
+  ];
+
+  return (
+    <>
+      <InfoSec>
+        <InfoRow>
+          <InfoColumn>
+            <TextWrapper>
+              <Heading>{cryptoDetails.name} Value Statistics</Heading>
+              <TopLine>
+                An overview showing the statistics of {cryptoDetails.name}, such
+                as the base and quote currency, the rank, and trading volume.
+              </TopLine>
+              {stats.map(({ title, value, icon }) => (
+                <TableRow>
+                  <Subtitle>{icon}</Subtitle>
+                  <Subtitle>{title}</Subtitle>
+                  <TableValue>{value}</TableValue>
+                </TableRow>
+              ))}
+            </TextWrapper>
+          </InfoColumn>
+
+          <InfoColumn>
+            <TextWrapper>
+              <Heading>{cryptoDetails.name} Value Statistics</Heading>
+              <TopLine>
+                An overview showing the statistics of {cryptoDetails.name}, such
+                as the base and quote currency, the rank, and trading volume.
+              </TopLine>
+              {stats.map(({ title, value, icon }) => (
+                <TableRow>
+                  <Subtitle>{icon}</Subtitle>
+                  <Subtitle>{title}</Subtitle>
+                  <TableValue>{value}</TableValue>
+                </TableRow>
+              ))}
+            </TextWrapper>
+          </InfoColumn>
+        </InfoRow>
+      </InfoSec>
+    </>
+  );
 };
 
 export default CryptoDetails;
---
+++ src/components/CryptoDetails/CryptoDetails.styles.js
@@ -0,0 +1,78 @@
+import styled from "styled-components";
+
+export const InfoSec = styled.div`
+  background-color: #eee;
+  color: #fff;
+  padding: 80px 0;
+`;
+
+export const InfoRow = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+`;
+
+export const InfoColumn = styled.div`
+  background-color: #0d0d36;
+  margin-bottom: 15px;
+  padding-right: 15px;
+  padding-left: 15px;
+  flex: 1;
+  max-width: 50%;
+  flex-basis: 50%;
+  border: 5px solid #eee;
+  display: flex;
+  justify-content: center;
+  @media screen and (max-width: 768px) {
+    max-width: 100%;
+    flex-basis: 100%;
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+  }
+`;
+
+export const TableRow = styled.div`
+  display: flex;
+  justify-content: start;
+`;
+
+export const TextWrapper = styled.div`
+  max-width: 540px;
+  padding-top: 60px;
+  padding-bottom: 60px;
+  @media screen and (max-width: 768px) {
+    padding-bottom: 65px;
+  }
+`;
+
+export const TopLine = styled.div`
+  color: #ccc;
+  font-size: 18px;
+  line-height: 16px;
+  font-weight: 700;
+  letter-spacing: 1.4px;
+  margin-bottom: 16px;
+`;
+
+export const Heading = styled.h1`
+  margin-bottom: 24px;
+  font-size: 24px;
+  line-height: 1.1;
+  font-weight: 600;
+  color: #fbc73c;
+`;
+
+export const Subtitle = styled.p`
+  max-width: 440px;
+  font-size: 18px;
+  line-height: 24px;
+  margin: 10px;
+`;
+
+export const TableValue = styled.p`
+  display: flex;
+  font-size: 20px;
+  line-height: 24px;
+  margin-left: auto;
+`;
